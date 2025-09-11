e81219ee90e167ba2f4c14bc207c6a5c2781ee2a
HEEJI YUN <93460560+Rodgershyeonsik@users.noreply.github.com>
2025-09-11 22:20:35 +0900

    study on recursion and stack(1)

---
+++ advanced-functions/recursion-and-stack.js
@@ -0,0 +1,34 @@
+// 재귀(Recursion) - 함수가 자기 자신을 호출하는 것
+// 큰 목표 작업 하나를 동일하면서도 간단한 작업 여러 개로 나눌 수 있을 때
+// 목표 작업을 간단한 동작 하나와 목표 작업을 변형한 작업으로 단순화시킬 수 있을 때
+
+// x를 n제곱해주는 함수 pow(x, n) 구현 예시
+// 1. for loop - 반복적 사고를 통한 방법
+function pow1(x, n) {
+    let result = 1;
+
+    for (let i = 0; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+console.log(pow1(2, 3)); // 8
+
+// 2. 재귀적 사고를 통한 방법
+// pow2는 n이 1이 될 때까지 재귀적으로 자신을 호출
+function pow2(x, n) {
+    if(n === 1) {
+        return x; // 명확한 결과값 즉시 도출. 재귀의 베이스(base)
+    } else {
+        return x * pow2(x, n - 1); // 재귀 단계(recursive step)
+    }
+}
+
+console.log(pow2(2, 3)); // 8
+
+// 3. 재귀로 더 짧게 구현한 버전
+function pow3(x, n) {
+    return (n === 1) ? x : x * pow3(x, n - 1);
+}
